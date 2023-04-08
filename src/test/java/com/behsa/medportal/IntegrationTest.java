7f66f002f0965f075caa3fd50de2ed5a2bc002c0
a.dashtbani <a.dashtbani@behsacorp.com>
2023-04-08 14:30:13 +0330

    change app package name

---
+++ src/test/java/com/behsa/medportal/IntegrationTest.java
@@ -0,0 +1,21 @@
+package com.behsa.medportal;
+
+import com.behsa.medportal.config.AsyncSyncConfiguration;
+import com.behsa.medportal.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { MedPortalApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}

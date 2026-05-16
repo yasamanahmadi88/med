8fa62361e735bdfa9395dfb0dbb10b163c2a38f7
y.ahmadi <y.ahmadi@behsacorp.com>
2026-05-16 11:53:50 +0330

    clean code

--- src/main/java/com/behsa/medportal/security/jwt/JWTFilter.java
+++ src/main/java/com/behsa/medportal/security/jwt/JWTFilter.java
@@ -1,18 +1,19 @@
 package com.behsa.medportal.security.jwt;
 
 import com.behsa.medportal.security.SecurityCache;
-import java.io.IOException;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * Filters incoming requests and installs a Spring Security principal

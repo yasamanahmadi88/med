17fad2780bacb9b5d114be706d5237f3b713e247
y.ahmadi <y.ahmadi@behsacorp.com>
2026-05-20 08:40:56 +0330

    added version controller build-prod

--- src/main/java/com/behsa/medportal/web/rest/VersionController.java
+++ src/main/java/com/behsa/medportal/web/rest/VersionController.java
@@ -1,5 +1,7 @@
 package com.behsa.medportal.web.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import java.util.Map;
 
 @RestController
 public class VersionController {
+    private final Logger log = LoggerFactory.getLogger(VersionController.class);
+
     private static final String version = "2.0.0-SNAPSHOT-mediation-";
 
 
